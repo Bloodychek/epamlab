d9671091a52969844c13b98d8b2dd3bf80844d5d
Евгений Егоров <andryushenka_egorov_2015@mail.ru>
2021-03-10 13:53:20 +0300

    fix

--- Classes2/src/Runner.java
+++ Classes2/src/Runner.java
@@ -4,14 +4,14 @@ import by.gsu.epamlab.Subject;
 public class Runner {
 
     public static void main(String[] args) {
-        Material material = new Material("steel", 7850);
-        Subject subject = new Subject("wire", material, 0.03);
+        Material steel = new Material("steel", 7850);
+        Subject subject = new Subject("wire", steel, 0.03);
 
-        System.out.println(material.toString());
+        System.out.println(steel.toString());
         System.out.println(subject.toString() + ";" + subject.getMass());
 
-        Material material1 = new Material("cooper", 8500);
-        subject.setMaterial(material1);
+        Material cooper = new Material("cooper", 8500);
+        subject.setMaterial(cooper);
         System.out.println("The wire mass is " + subject.getMass());
     }
 }
