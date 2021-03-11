82a07fdc6e0f52843c779332dc396cde36c72de0
Евгений Егоров <andryushenka_egorov_2015@mail.ru>
2021-03-11 23:06:00 +0300

    fix

---
+++ classes2/src/by/gsu/epamlab/Subject.java
@@ -0,0 +1,51 @@
+package by.gsu.epamlab;
+
+public class Subject {
+    private String name;
+    private Material material;
+    private Double volume;
+
+    public Subject(){
+
+    }
+
+    public Subject(String name, Material material, Double volume) {
+        this.name = name;
+        this.material = material;
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(Material material) {
+        this.material = material;
+    }
+
+    public Double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(Double volume) {
+        this.volume = volume;
+    }
+
+    public Double getMass()
+    {
+        return material.getDensity() * volume;
+    }
+
+    @Override
+    public String toString() {
+        return name + ";" + material + ";" +  volume + ";" + getMass();
+    }
+}
