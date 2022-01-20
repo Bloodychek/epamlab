6157c2df08a18d423bc440d020662bf016b9da61
Bloodychek <andryushenka_egorov_2015@mail.ru>
2022-01-20 20:08:06 +0300

    fix1

--- jdbc1/src/by/gsu/epamlab/beans/Constants.java
+++ jdbc1/src/by/gsu/epamlab/beans/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     public static final String PASSWORD = "1234";
     public static final String DELIMITER = ";";
     public static final String SELECT_LEN = "Select len, num from frequencies where len > num";
-    public static final String DELETE = "delete from frequencies where IdFrequencies > 0";
+    public static final String DELETE = "delete from frequencies";
     public static final String SELECT = "select abs(round(x2 - x1)) as len, count(*) as num from coordinates group by len order by len";
     public static final String INSERT = "insert frequencies(len, num) values (?, ?)";
 }
